05519d6136f4834b2881acc9d844a4061b9c6665
Yugeng Hui <interhuiyg@163.com>
2015-08-25 21:50:23 +0800

    Add node_reader

---
+++ src/ndb/common/node.go
@@ -0,0 +1,68 @@
+package common
+
+import (
+	"strings"
+)
+
+
+type Node struct {
+	name string
+	values map[string][]string
+	children []*Node
+}
+
+func (node *Node) SetName(name string) {
+	node.name = name
+}
+
+func (node *Node) GetName() string {
+	return node.name
+}
+
+func (node *Node) GetValues() map[string][]string {
+	return node.values
+}
+
+func (node *Node) FindChildByName(name string) []*Node {
+	if name == "" {
+		return nil
+	}
+	children := node.children
+	if children != nil {
+		matchedChildren := []*Node{}
+		for _, child := range children {
+			if child != nil && child.name == name {
+				matchedChildren = append(matchedChildren, child)
+			}
+		}
+		return matchedChildren
+	}
+	return nil
+}
+
+func (node *Node) AddChild(child *Node) {
+	node.children = append(node.children, child)
+}
+
+func (node *Node) GetChileren() []*Node {
+	return node.children
+}
+
+func (node *Node) GetValue(key string) []string {
+	value := node.values
+	return value[key]
+}
+
+func (node *Node) GetValueString(key string) string {
+	value := node.GetValue(key)
+	return strings.Join(value, ",")
+}
+
+func (node *Node) SetValue(key string, value []string) {
+	nodeValue := node.values
+	if (nodeValue == nil) {
+		nodeValue = make(map[string][]string)
+	}
+	nodeValue[key] = value
+	node.values = nodeValue
+}
---
+++ src/ndb/common/node_reader.go
@@ -0,0 +1,101 @@
+package common
+
+import (
+	"bufio"
+	"io"
+    "os/exec"
+    "path/filepath"
+	"os"
+	"strings"
+	"errors"
+)
+
+func GetCurrPath() string {
+    file, _ := exec.LookPath(os.Args[0])
+    path, _ := filepath.Abs(file)
+    pathSplit := strings.Split(path, "\\")
+    size := len(pathSplit)
+    pathSplit = strings.Split(path, pathSplit[size-1])
+    currPath := strings.Replace(pathSplit[0], "\\", "/", size-1)
+    return currPath
+}
+
+func Read(filename string) (*Node, error) {
+	if filename == "" {
+		return nil, errors.New("Filename is NULL")
+	}
+	if !strings.Contains(filename, "\\") &&  !strings.Contains(filename, "/") {
+		filename = GetCurrPath() + "/" + filename
+	}
+	content, err := ReadFile(filename)
+	if err == nil {
+		node, _ := Parse(0, content, nil)
+		return node, nil
+	} else {
+		return nil, err
+	}
+}
+
+
+func ReadFile(filename string) ([]string, error) {
+	fi, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+
+	result := []string{}
+
+	bfRd := bufio.NewReader(fi)
+	for {
+		line, err := bfRd.ReadBytes('\n')
+		result = append(result, string(line))
+
+		if err != nil { 
+			if err == io.EOF {
+				return result, nil
+			}
+			return result, err
+		}
+	}
+	return result, nil
+}
+
+func Parse(linenum int, contents []string, parent *Node) (*Node, int) {
+	if parent == nil {
+		parent = new(Node)
+	}
+
+	if contents != nil {
+		for i := linenum; i < len(contents); i++ {
+			line := strings.TrimSpace(contents[i])
+			if line == "" || strings.HasPrefix(line, "#") {
+				continue
+			}
+			if strings.HasSuffix(line, "{") {
+				node := new(Node)
+				node.name = strings.TrimSpace(line[:strings.LastIndex(line, "{")])
+				nodeChild, _line := Parse(i + 1, contents, node)
+				parent.AddChild(nodeChild)
+				
+				i = _line
+			} else if strings.HasPrefix(line, "}") {
+				return parent, i
+			} else {
+				if strings.Index(line, ":") > 0 {
+					itemName := strings.TrimSpace(line[:strings.Index(line, ":")])
+					itemValue := strings.TrimSpace(line[strings.Index(line, ":") + 1 : len(line)])
+					
+					valueList := parent.GetValue(itemName)
+					if valueList != nil {
+						valueList = []string{}
+					}
+					valueList = append(valueList, itemValue)
+					parent.SetValue(itemName, valueList)
+				}
+			}
+		}
+	}
+	
+	return parent, len(contents)
+}

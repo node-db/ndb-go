43c464f7460678f888c5f6a6d9df8a981f124124
Yugeng Hui <interhuiyg@163.com>
2015-10-18 18:56:09 +0800

    Update PrintNode for beauty format print

--- src/ndb_cli/ndb.go
+++ src/ndb_cli/ndb.go
@@ -4,54 +4,82 @@ import (
 	"ndb"
 	"flag"
 	"fmt"
-	"strings"
 )
 
 func main() {
 	filename := flag.String("f", "" , "Use -f <ndb file path>")
 	query := flag.String("q", "", "Use -q <ndb query>")
-	help := flag.Bool("h", false, "Use -h <help message>")
+	help := flag.Bool("h", false, "Use -h")
 	
 	flag.Usage = PrintHelp
 	flag.Parse()
 	
-	
 	if *help {
 		PrintHelp()
+		return
 	}
 	
-	if *filename != "" && *query != "" {
-		node, err := ndb.Read(*filename)
+	// 当执行出现异常(panic)时, 输出错误信息 
+	defer func() {
+		if err := recover(); err != nil {  
+			fmt.Println("Program Error")
+		}
+	}()
+	
+	if *filename != "" {
+		node, err := ndb.ReadFile(*filename)
 		if err == nil {
-			result, found := ndb.Execute(node, *query)
-			if found {
-				switch result.(type) { 
-				case []*ndb.Node:
-					list := result.([]*ndb.Node)
-					for _, item := range list {
-						PrintNode(item)
+			if *query != "" {
+				result, found, err := ndb.Execute(node, *query)
+				if err != nil {
+					fmt.Println("Query Error: " + *query)
+				}
+				if found {
+					switch result.(type) { 
+					case []*ndb.Node:
+						list := result.([]*ndb.Node)
+						for _, item := range list {
+							PrintNode(item, 0)
+						}
+					case *ndb.Node:
+						PrintNode(result.(*ndb.Node), 0)
 					}
-				case *ndb.Node:
-					PrintNode(result.(*ndb.Node))
+				} else {
+					fmt.Printf("%s Found NOTHING\n", *query)	
 				}
 			} else {
-				fmt.Printf("%s Found NOTHING\n", *query)	
+				PrintNode(node, 0)
 			}
 		} else {
-			fmt.Printf("Read file %s FAIL\n", *filename)	
+			fmt.Printf("Read file %s FAIL\n", *filename)
 		}
 	} else {
 		PrintHelp()
 	}
 }
 
-func PrintNode(node *ndb.Node) {
-	tab := "|--"
+func PrintNode(node *ndb.Node, deep int) {
 	
-	fmt.Println(node.GetName())
-	values := node.GetValues();
+	tab := ""
+	for i := 0 ; i < deep; i++ {
+		tab += "   "
+	}
+	
+	nodeName := node.GetName()
+	if nodeName != "" {
+		fmt.Println(tab + nodeName)
+	}
+
+	values := node.GetValues()
 	for key, value := range values {
-		fmt.Println(tab + key + " : " + strings.Join(value, ","))
+		for _, valueItem := range value {
+			fmt.Println(tab + "|--" + key + " : " + valueItem)
+		}
+	}
+	
+	children := node.GetChileren()
+	for _, child := range children {
+		PrintNode(child, deep + 1)
 	}
 }
 
@@ -61,3 +89,4 @@ func PrintHelp() {
 	fmt.Println("-q\tndb query\texample: select:root->parent->child->name:jim")
 	fmt.Println("-h\tshow help message")
 }
+
